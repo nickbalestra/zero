c282427b925a6def4cf2b7c8cdba293a4ccd01f4
Nick Balestra <nbalestra@opentable.com>
2017-12-25 16:57:55 +0000

    strategy constructing tests

--- __tests__/strategy.js
+++ __tests__/strategy.js
@@ -1,23 +1,55 @@
 const Strategy = require('../lib/Strategy.js');
-
-test('strategy name', () => {
-  const strategy = new Strategy(
-    {
-      deliver: {},
-      secret: 'very-secret'
-    },
-    () => {}
-  );
-  expect(strategy.name).toBe('zero');
-});
+const tokenStorage = require('../lib/tokenStorage');
 
 test('should throw if constructed without a verify callback', () => {
-  // expect(new Strategy()).toBe('')
-  // expect(function() {
-  //   var s = new Strategy();
-  // }).to.throw(TypeError, 'LocalStrategy requires a verify callback');
+  expect(() => {
+    new Strategy({
+      secret: 'top-secret',
+      deliver: {}
+    });
+  }).toThrowError('ZeroStrategy requires a verify callback');
 });
 
-test('should throw if constructed without a secret option', () => {});
+test('should throw if constructed without a secret', () => {
+  expect(() => {
+    new Strategy(
+      {
+        deliver: {}
+      },
+      () => {}
+    );
+  }).toThrowError('ZeroStrategy requires a secret');
+});
 
-test('should throw if constructed without a deliver option', () => {});
+test('should throw if constructed without a deliver', () => {
+  expect(() => {
+    new Strategy(
+      {
+        secret: 'top-secret'
+      },
+      () => {}
+    );
+  }).toThrowError('ZeroStrategy requires a deliver module');
+});
+
+test('strategy defaults', () => {
+  let stategy;
+  const verify = () => {};
+  expect(() => {
+    strategy = new Strategy(
+      {
+        secret: 'top-secret',
+        deliver: {
+          addressField: 'email',
+          tokenField: 'token'
+        }
+      },
+      verify
+    );
+  }).not.toThrow();
+  expect(strategy.name).toBe('zero');
+  expect(strategy.secret).toBe('top-secret');
+  expect(strategy.verify).toBe(verify);
+  expect(strategy.storage).toBe(tokenStorage);
+  expect(strategy.ttl).toBe(604800);
+});
